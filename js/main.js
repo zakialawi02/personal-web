a2c1f1033d42a407f8da8b422d7b9b9329a9b0ef
Ahmad zaki Alawi <89363468+zakialawi02@users.noreply.github.com>
2021-12-21 01:12:34 +0700

    Add files via upload
    navbar fix1

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// sticky header 
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+// 
