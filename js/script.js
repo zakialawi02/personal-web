da5eaa003892ad0283ebc366cbbaaf5d0513e2fd
Ahmad zaki Alawi <89363468+zakialawi02@users.noreply.github.com>
2021-12-29 11:42:21 +0700

    Nambah Form validaton/condition (#4)
    Nambah Form validaton/condition, sudah final tinggal ganti photo profil

--- js/script.js
+++ js/script.js
@@ -313,37 +313,8 @@ twitterBtn.addEventListener("click", () => {
 quoteBtn.addEventListener("click", randomQuote);
 
 //------------------ Working contact form
-// const form = document.querySelector("form"),
-//   statusTxt = form.querySelector("input.btn-contact span");
+// 
 
-// form.onsubmit = (e) => {
-//   e.preventDefault();
-//   statusTxt.style.color = "#0D6EFD";
-//   statusTxt.style.display = "block";
-//   // statusTxt.innerText = "Sending your message...";
-//   statusTxt.innerText = "Contact Form Can't Use Right Now.";
-//   form.classList.add("disabled");
-
-//   let xhr = new XMLHttpRequest();
-//   xhr.open("POST", "mail.php", true);
-//   xhr.onload = () => {
-//     if (xhr.readyState == 4 && xhr.status == 200) {
-//       let response = xhr.response;
-//       if (response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") != -1 || response.indexOf("Sorry, failed to send your message!") != -1) {
-//         statusTxt.style.color = "red";
-//       } else {
-//         form.reset();
-//         setTimeout(() => {
-//           statusTxt.style.display = "none";
-//         }, 3000);
-//       }
-//       statusTxt.innerText = response;
-//       form.classList.remove("disabled");
-//     }
-//   }
-//   let formData = new FormData(form);
-//   xhr.send(formData);
-// }
 
 // // ================= Dark Mode
 // const chk = document.getElementById('chk');
