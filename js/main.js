70b0fa228864894722d5156600f88061d72e0a83
Ahmad zaki Alawi <89363468+zakialawi02@users.noreply.github.com>
2022-01-01 01:38:28 +0700

    Delete main.js

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// sticky header 
-window.addEventListener("scroll", function () {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
-// 
